29c348d276572b7543880c1a45dc5de2fabf64a0
Mongreezy <gamefreak148d@gmail.com>
2026-06-30 14:31:37 +0200

    feat: implement name tag generator with camelCase variables

---
+++ 2.digitalNameTag.py
@@ -0,0 +1,9 @@
+# 2. The Digital Name Tag
+# Python uses snake_case
+
+firstName = input("Enter first name:\n")
+lastName = input("Enter last name:\n")
+jobTitle = input("Enter job title:\n")
+
+print("********** Name: " + firstName + " " + lastName + " **********")
+print("********** Title: " + jobTitle + " **********")
